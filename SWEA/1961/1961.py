07ed063c927a886c7a718fb217f29736ae0fcfcb
notrealsilk <kmjk1117@gmail.com>
2024-08-12 01:45:19 +0900

    Update 1961.py

--- SWEA/1961/1961.py
+++ SWEA/1961/1961.py
@@ -1,6 +1,31 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+def deg_90(N,N_list):
+    deg_90 = [[0]*N for _ in range(N)]
+
+    for i in range(N): # 행
+        for j in range(N):
+            deg_90[i][j] = N_list[N-1-j][i]
+    return deg_90
+
+def deg_180(N,N_list):
+    deg_180 = [[0] * N for _ in range(N)]
+    for i in range(N): # 행
+        for j in range(N):
+            deg_180[i][j] = N_list[N-1-i][N-1-j]
+    return deg_180
+
+def deg_270(N,N_list):
+    deg_270 = [[0]*N for _ in range(N)]
+
+    for i in range(N): # 행
+        for j in range(N):
+            deg_270[i][j] = N_list[j][N-1-i]
+    return deg_270
+
+
+
 T = int(input())
 
 for tc in range(1, T+1):
@@ -11,9 +36,15 @@ for tc in range(1, T+1):
 
     # 각도별 회전한 모양
     # 2차원 리스트로 값을 갱신하고 최종값은 연속해서 출력
-    deg_90, deg_180, deg_270 = [],[],[]
-    #print(N_list)
+    matrix_90, matrix_180, matrix_270 = deg_90(N,N_list),deg_180(N,N_list),deg_270(N,N_list)
 
+    print(f"{tc}")
+    for row in matrix_90:
+        print(''.join(map(str, row)))
+    for row in matrix_180:
+        print(''.join(map(str, row)))
+    for row in matrix_270:
+        print(''.join(map(str, row)))
 
 
     """
