ab512954dcab477c3b6fe8bf041d44d5667b8a53
notrealsilk <kmjk1117@gmail.com>
2024-09-08 23:20:22 +0900

    Update list2_selection_sort.py

--- Algorithm_aps/list/list2_selection_sort.py
+++ Algorithm_aps/list/list2_selection_sort.py
@@ -1,14 +1,15 @@
-def selection_sort (arr, N): # arr..정렬 대상, N 크기
+def selection_sort(arr, N): # arr..정렬 대상, N 크기
     for i in range(N-1): # 주어진 구간에 대해.. 기준위치를 i를 정하고
         min_idx = i # 최솟값 위치를 기준위치로 가정
         for j in range(i+1, N): # 남은 원소에 대해 실제 최솟값 위치 검색
             if arr[min_idx] > arr[j]:
-                min_idx = [j]
-        arr[i], arr[min_idx] = arr[min_idx], arr # 구간의 최솟값을 교환
-
-
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i] # 구간의 최솟값을 교환
 
 A = [2,7,5,3,4]
 B = [4,3,2,1]
 selection_sort(A, len(A))
 selection_sort(B, len(B))
+
+print(A) # [2, 3, 4, 5, 7]
+print(B) # [1, 2, 3, 4]
